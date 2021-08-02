e278c354fcfef522be61cd5e058b3a4d4fe74102
root <sammy5390@yahoo.com>
2021-08-02 12:01:32 +0800

    galaxy

--- Galaxy.py
+++ Galaxy.py
@@ -1,12 +1,16 @@
 import pygame
+import random
 
 FPS=60
 
 WIDTH=500
 HEIGHT=600
 
+BLACK=(0,0,0)
 WHITE=(255,255,255)
 GREEN=(0,255,0)
+RED=(255,0,0)
+YELLOW=(255,255,0)
 
 pygame.init()
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
@@ -34,11 +38,54 @@ class Player(pygame.sprite.Sprite):
 		if self.rect.left<0:
 			self.rect.left=0
 			
+	def shoot(self):
+		bullet=Bullet(self.rect.centerx, self.rect.top)
+		all_sprites.add(bullet)
+			
+class Rock(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.image=pygame.Surface((30,40))
+		self.image.fill(RED)
+		self.rect=self.image.get_rect()
+		self.rect.x=random.randrange(0,WIDTH- self.rect.width)
+		self.rect.y=random.randrange(-100,-40)
+		self.speedy=random.randrange(2,10)
+		self.speedx=random.randrange(-3,3)
+		
+	def update(self):
+		self.rect.y +=self.speedy
+		self.rect.x +=self.speedx
+		if self.rect.top>HEIGHT or self.rect.left>WIDTH or self.rect.right<0 :
+			self.rect.x=random.randrange(0,WIDTH- self.rect.width)
+			self.rect.y=random.randrange(-100,-40)
+			self.speedy=random.randrange(2,10)
+			self.speedx=random.randrange(-3,3)
+			
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self,x,y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image=pygame.Surface((10,20))
+		self.image.fill(YELLOW)
+		self.rect=self.image.get_rect()
+		self.rect.centerx=x
+		self.rect.bottom=y
+		self.speedy=-10
+
+		
+	def update(self):
+		self.rect.y +=self.speedy
+		if self.rect.bottom<0:
+			self.kill()
+
 
 		
 all_sprites=pygame.sprite.Group()
 player=Player()
 all_sprites.add(player)
+for i in range(8):
+	r=Rock()
+	all_sprites.add(r)
 
 
 running=True
@@ -50,13 +97,16 @@ while running:
 	for event in pygame.event.get():
 		if event.type==pygame.QUIT:
 			running=False
+		elif event.type==pygame.KEYDOWN:
+			if event.key==pygame.K_SPACE:
+				player.shoot()
 			
 	# update
 	all_sprites.update()
 	
 	
 	# display
-	screen.fill(WHITE)
+	screen.fill(BLACK)
 	all_sprites.draw(screen)
 	pygame.display.update()
 			
